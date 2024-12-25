bfea7ae246c6c4d974ef5674538c5d87acf874e9
nitroglycerine <suichen6@gmail.com>
2024-12-24 16:19:19 -0800

    Use fullscreen quad.

--- main.cpp
+++ main.cpp
@@ -24,6 +24,7 @@
 #pragma comment(lib, "dxgi.lib")
 
 #include <d3d12.h>
+#include <d3dcompiler.h>
 #include <dxgi1_4.h>
 #include <dxcapi.h>
 #include <DirectXMath.h>
@@ -123,9 +124,16 @@ ID3D12GraphicsCommandList4* g_command_list1;  // For building AS
 ID3D12RootSignature*         g_global_rootsig{};
 ID3D12StateObject*           g_rt_state_object;
 ID3D12StateObjectProperties* g_rt_state_object_props;
+
 ID3D12RootSignature*         g_global_rootsig_ao{};
 ID3D12StateObject*           g_rt_state_object_ao;
 ID3D12StateObjectProperties* g_rt_state_object_props_ao;
+
+ID3D12RootSignature* g_rootsig_fsquad{};
+ID3D12PipelineState* g_pipeline_fsquad{};
+ID3D12DescriptorHeap* g_srv_uav_cbv_heap_fsquad{};
+ID3D12Resource*       g_fsquad_vb;
+D3D12_VERTEX_BUFFER_VIEW g_fsquad_vbv;
 
 glm::mat4 g_inv_view;
 glm::mat4 g_inv_proj;
@@ -447,13 +455,12 @@ void InitDX12Stuff()
     desc.Width     = WIN_W * WIN_H * sizeof(float) * 4;
     desc.Height    = 1;
     desc.Layout    = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
-    CE(g_device12->CreateCommittedResource(
-        &props, D3D12_HEAP_FLAG_NONE, &desc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&g_hitpos_ao)));
+    CE(g_device12->CreateCommittedResource(&props, D3D12_HEAP_FLAG_NONE, &desc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&g_hitpos_ao)));
     g_hitpos_ao->SetName(L"Hit position");
 
     // CBV SRV UAV Heap
     D3D12_DESCRIPTOR_HEAP_DESC heap_desc{};
-    heap_desc.NumDescriptors = 6;  // [0]=output, [1]=BVH, [2]=CBV, [3]=Verts, [4]=Offsets
+    heap_desc.NumDescriptors = 6;  // [0]=output, [1]=BVH, [2]=CBV, [3]=Verts, [4]=Offsets, [5]=HitNormal
     heap_desc.Type           = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
     heap_desc.Flags          = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
     CE(g_device12->CreateDescriptorHeap(&heap_desc, IID_PPV_ARGS(&g_srv_uav_cbv_heap)));
@@ -462,8 +469,8 @@ void InitDX12Stuff()
 
     // Query heap
     D3D12_QUERY_HEAP_DESC qhd{};
-    qhd.Count = 2;
-    qhd.Type  = D3D12_QUERY_HEAP_TYPE_TIMESTAMP;
+    qhd.Count    = 2;
+    qhd.Type     = D3D12_QUERY_HEAP_TYPE_TIMESTAMP;
     qhd.NodeMask = 0;
     CE(g_device12->CreateQueryHeap(&qhd, IID_PPV_ARGS(&g_query_heap)));
 
@@ -476,20 +483,196 @@ void InitDX12Stuff()
     CE(g_device12->CreateCommittedResource(
         &props, D3D12_HEAP_FLAG_NONE, &desc, D3D12_RESOURCE_STATE_COPY_DEST, nullptr, IID_PPV_ARGS(&g_query_readback_buffer)));
 
-    // UAV
+    // UAV of RT output resource
     D3D12_CPU_DESCRIPTOR_HANDLE      handle(g_srv_uav_cbv_heap->GetCPUDescriptorHandleForHeapStart());
     D3D12_UNORDERED_ACCESS_VIEW_DESC uav_desc{};
     uav_desc.ViewDimension = D3D12_UAV_DIMENSION_TEXTURE2D;
     g_device12->CreateUnorderedAccessView(g_rt_output_resource, nullptr, &uav_desc, handle);
 
     handle.ptr += 5 * g_srv_uav_cbv_descriptor_size;
-    uav_desc.ViewDimension = D3D12_UAV_DIMENSION_BUFFER;
+    uav_desc.ViewDimension               = D3D12_UAV_DIMENSION_BUFFER;
     uav_desc.Buffer.CounterOffsetInBytes = 0;
     uav_desc.Buffer.FirstElement         = 0;
     uav_desc.Buffer.Flags                = D3D12_BUFFER_UAV_FLAG_NONE;
     uav_desc.Buffer.NumElements          = WIN_W * WIN_H;
     uav_desc.Buffer.StructureByteStride  = sizeof(float) * 4;
     g_device12->CreateUnorderedAccessView(g_hitpos_ao, nullptr, &uav_desc, handle);
+
+    // Root params for drawing the FSQUAD
+    {
+        D3D12_ROOT_PARAMETER root_params[1];
+        root_params[0].ParameterType    = D3D12_ROOT_PARAMETER_TYPE_DESCRIPTOR_TABLE;
+        root_params[0].ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL;
+
+        D3D12_DESCRIPTOR_RANGE desc_ranges[1]{};
+        desc_ranges[0].RangeType                         = D3D12_DESCRIPTOR_RANGE_TYPE_SRV;  // RT output viewed as SRV
+        desc_ranges[0].NumDescriptors                    = 1;
+        desc_ranges[0].BaseShaderRegister                = 0;
+        desc_ranges[0].RegisterSpace                     = 0;
+        desc_ranges[0].OffsetInDescriptorsFromTableStart = D3D12_DESCRIPTOR_RANGE_OFFSET_APPEND;
+
+        root_params[0].DescriptorTable.NumDescriptorRanges = _countof(desc_ranges);
+        root_params[0].DescriptorTable.pDescriptorRanges   = desc_ranges;
+
+        D3D12_STATIC_SAMPLER_DESC sampler_desc{};
+        sampler_desc.ShaderRegister = 0;
+        sampler_desc.ShaderVisibility = D3D12_SHADER_VISIBILITY_PIXEL;
+        sampler_desc.Filter           = D3D12_FILTER_ANISOTROPIC;
+        sampler_desc.AddressU         = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        sampler_desc.AddressV         = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        sampler_desc.AddressW         = D3D12_TEXTURE_ADDRESS_MODE_WRAP;
+        sampler_desc.MipLODBias       = 0;
+        sampler_desc.MaxAnisotropy    = 8;
+        sampler_desc.ComparisonFunc   = D3D12_COMPARISON_FUNC_LESS_EQUAL;
+        sampler_desc.BorderColor      = D3D12_STATIC_BORDER_COLOR_OPAQUE_WHITE;
+        sampler_desc.MinLOD           = 0.0f;
+        sampler_desc.MaxLOD           = D3D12_FLOAT32_MAX;
+        sampler_desc.RegisterSpace    = 0;
+
+        D3D12_ROOT_SIGNATURE_DESC rootsig_desc{};
+        rootsig_desc.Flags             = D3D12_ROOT_SIGNATURE_FLAG_ALLOW_INPUT_ASSEMBLER_INPUT_LAYOUT;
+        rootsig_desc.NumParameters     = 1;
+        rootsig_desc.pParameters       = root_params;
+        rootsig_desc.NumStaticSamplers = 1;
+        rootsig_desc.pStaticSamplers   = &sampler_desc;
+
+        ID3DBlob *signature{}, *error{};
+        D3D12SerializeRootSignature(&rootsig_desc, D3D_ROOT_SIGNATURE_VERSION_1, &signature, &error);
+        if (error)
+        {
+            printf("Error: %s\n", (char*)(error->GetBufferPointer()));
+        }
+        CE(g_device12->CreateRootSignature(0, signature->GetBufferPointer(), signature->GetBufferSize(), IID_PPV_ARGS(&g_rootsig_fsquad)));
+        g_rootsig_fsquad->SetName(L"FSQuad root signature");
+        signature->Release();
+        if (error)
+            error->Release();
+    }
+
+    // PSO for fullscreen quad
+    {
+        ID3DBlob *vs_blob, *ps_blob, *error;
+        unsigned  compile_flags = 0;
+        D3DCompileFromFile(L"shaders/fsquad.hlsl", nullptr, nullptr, "VSMain", "vs_5_0", compile_flags, 0, &vs_blob, &error);
+        if (error)
+            printf("Error building VS: %s\n", (char*)(error->GetBufferPointer()));
+
+        D3DCompileFromFile(L"shaders/fsquad.hlsl", nullptr, nullptr, "PSMain", "ps_5_0", compile_flags, 0, &ps_blob, &error);
+        if (error)
+            printf("Error building PS: %s\n", (char*)(error->GetBufferPointer()));
+
+        D3D12_INPUT_ELEMENT_DESC input_element_descs[] = {{"POSITION", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 0, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA},
+                                                          {"TEXCOORD", 0, DXGI_FORMAT_R32G32_FLOAT, 0, 8, D3D12_INPUT_CLASSIFICATION_PER_VERTEX_DATA}};
+
+        D3D12_GRAPHICS_PIPELINE_STATE_DESC pso_desc{};
+        pso_desc.pRootSignature     = g_rootsig_fsquad;
+        pso_desc.VS.pShaderBytecode = vs_blob->GetBufferPointer();
+        pso_desc.VS.BytecodeLength  = vs_blob->GetBufferSize();
+        pso_desc.PS.pShaderBytecode                                       = ps_blob->GetBufferPointer();
+        pso_desc.PS.BytecodeLength                                        = ps_blob->GetBufferSize();
+        const D3D12_RENDER_TARGET_BLEND_DESC defaultRenderTargetBlendDesc = {
+            FALSE,
+            FALSE,
+            D3D12_BLEND_ONE,
+            D3D12_BLEND_ZERO,
+            D3D12_BLEND_OP_ADD,
+            D3D12_BLEND_ONE,
+            D3D12_BLEND_ZERO,
+            D3D12_BLEND_OP_ADD,
+            D3D12_LOGIC_OP_NOOP,
+            D3D12_COLOR_WRITE_ENABLE_ALL,
+        };
+        pso_desc.BlendState.AlphaToCoverageEnable  = false;
+        pso_desc.BlendState.IndependentBlendEnable = false;
+        for (unsigned i = 0; i < D3D12_SIMULTANEOUS_RENDER_TARGET_COUNT; i++)
+        {
+            pso_desc.BlendState.RenderTarget[i] = defaultRenderTargetBlendDesc;
+        }
+        pso_desc.SampleMask                               = UINT_MAX;
+        pso_desc.RasterizerState.FillMode                 = D3D12_FILL_MODE_SOLID;
+        pso_desc.RasterizerState.CullMode                 = D3D12_CULL_MODE_NONE;
+        pso_desc.RasterizerState.FrontCounterClockwise    = FALSE;
+        pso_desc.RasterizerState.DepthBias                = D3D12_DEFAULT_DEPTH_BIAS;
+        pso_desc.RasterizerState.DepthBiasClamp           = D3D12_DEFAULT_DEPTH_BIAS_CLAMP;
+        pso_desc.RasterizerState.SlopeScaledDepthBias     = D3D12_DEFAULT_SLOPE_SCALED_DEPTH_BIAS;
+        pso_desc.RasterizerState.DepthClipEnable          = TRUE;
+        pso_desc.RasterizerState.MultisampleEnable        = FALSE;
+        pso_desc.RasterizerState.AntialiasedLineEnable    = FALSE;
+        pso_desc.RasterizerState.ForcedSampleCount        = 0;
+        pso_desc.RasterizerState.ConservativeRaster       = D3D12_CONSERVATIVE_RASTERIZATION_MODE_OFF;
+        pso_desc.DepthStencilState.DepthEnable            = TRUE;
+        pso_desc.DepthStencilState.DepthWriteMask         = D3D12_DEPTH_WRITE_MASK_ALL;
+        pso_desc.DepthStencilState.DepthFunc              = D3D12_COMPARISON_FUNC_LESS;
+        pso_desc.DepthStencilState.StencilEnable          = FALSE;
+        pso_desc.DepthStencilState.StencilReadMask        = D3D12_DEFAULT_STENCIL_READ_MASK;
+        pso_desc.DepthStencilState.StencilWriteMask       = D3D12_DEFAULT_STENCIL_WRITE_MASK;
+        const D3D12_DEPTH_STENCILOP_DESC defaultStencilOp = {D3D12_STENCIL_OP_KEEP, D3D12_STENCIL_OP_KEEP, D3D12_STENCIL_OP_KEEP, D3D12_COMPARISON_FUNC_ALWAYS};
+        pso_desc.DepthStencilState.FrontFace              = defaultStencilOp;
+        pso_desc.DepthStencilState.BackFace               = defaultStencilOp;
+        pso_desc.InputLayout.pInputElementDescs           = input_element_descs;
+        pso_desc.InputLayout.NumElements                  = _countof(input_element_descs);
+        pso_desc.PrimitiveTopologyType                    = D3D12_PRIMITIVE_TOPOLOGY_TYPE_TRIANGLE;
+        pso_desc.NumRenderTargets                         = 1;
+        pso_desc.RTVFormats[0]                            = DXGI_FORMAT_R8G8B8A8_UNORM;
+        pso_desc.DSVFormat                                = DXGI_FORMAT_D32_FLOAT;
+        pso_desc.SampleDesc.Count                         = 1;
+        CE(g_device12->CreateGraphicsPipelineState(&pso_desc, IID_PPV_ARGS(&g_pipeline_fsquad)));
+        g_pipeline_fsquad->SetName(L"FSQuad pipeline");
+
+        D3D12_DESCRIPTOR_HEAP_DESC heap_desc{};
+        heap_desc.NumDescriptors = 1;  // [0]=rt_output's SRV
+        heap_desc.Type           = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV;
+        heap_desc.Flags          = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE;
+        CE(g_device12->CreateDescriptorHeap(&heap_desc, IID_PPV_ARGS(&g_srv_uav_cbv_heap_fsquad)));
+
+        // SRV of RT output resource
+        D3D12_SHADER_RESOURCE_VIEW_DESC srv_desc{};
+        srv_desc.Shader4ComponentMapping   = D3D12_DEFAULT_SHADER_4_COMPONENT_MAPPING;
+        srv_desc.ViewDimension             = D3D12_SRV_DIMENSION_TEXTURE2D;
+        srv_desc.Format                    = DXGI_FORMAT_R8G8B8A8_UNORM;
+        srv_desc.Texture2D.MipLevels       = 1;
+        srv_desc.Texture2D.MostDetailedMip = 0;
+        D3D12_CPU_DESCRIPTOR_HANDLE srv_handle(g_srv_uav_cbv_heap_fsquad->GetCPUDescriptorHandleForHeapStart());
+        g_device12->CreateShaderResourceView(g_rt_output_resource, &srv_desc, srv_handle);
+
+        // Vertex buffer of FSQUAD
+        float verts[][4] = {
+            {-1, 1, 0, 0},  // top-left
+            {3, 1, 2, 0},   // top-right
+            {-1, -3, 0, 2}  // bottom-left
+        };
+        D3D12_RESOURCE_DESC res_desc{};
+        res_desc.Dimension          = D3D12_RESOURCE_DIMENSION_BUFFER;
+        res_desc.Alignment          = 0;
+        res_desc.Height             = 1;
+        res_desc.DepthOrArraySize   = 1;
+        res_desc.MipLevels          = 1;
+        res_desc.Format             = DXGI_FORMAT_UNKNOWN;
+        res_desc.SampleDesc.Count   = 1;
+        res_desc.SampleDesc.Quality = 0;
+        res_desc.Layout             = D3D12_TEXTURE_LAYOUT_ROW_MAJOR;
+        res_desc.Flags              = D3D12_RESOURCE_FLAG_NONE;
+        res_desc.Width              = sizeof(verts);
+
+        D3D12_HEAP_PROPERTIES heap_props{};
+        heap_props.Type                 = D3D12_HEAP_TYPE_UPLOAD;
+        heap_props.CPUPageProperty      = D3D12_CPU_PAGE_PROPERTY_UNKNOWN;
+        heap_props.MemoryPoolPreference = D3D12_MEMORY_POOL_UNKNOWN;
+        heap_props.CreationNodeMask     = 1;
+        heap_props.VisibleNodeMask      = 1;
+
+        ID3D12Resource* d_all_verts;
+        CE(g_device12->CreateCommittedResource(
+            &heap_props, D3D12_HEAP_FLAG_NONE, &res_desc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&g_fsquad_vb)));
+        char* mapped;
+        g_fsquad_vb->Map(0, nullptr, (void**)(&mapped));
+        memcpy(mapped, verts, sizeof(verts));
+        g_fsquad_vb->Unmap(0, nullptr);
+
+        g_fsquad_vbv.BufferLocation = g_fsquad_vb->GetGPUVirtualAddress();
+        g_fsquad_vbv.SizeInBytes    = sizeof(verts);
+        g_fsquad_vbv.StrideInBytes  = sizeof(float) * 4;
+    }
 }
 
 void CreateRTPipeline()
@@ -798,6 +981,7 @@ void CreateShaderBindingTable()
     g_raygen_sbt_storage->Map(0, nullptr, (void**)&mapped);
     memcpy(mapped, raygen_shader_id, D3D12_SHADER_IDENTIFIER_SIZE_IN_BYTES);
     g_raygen_sbt_storage->Unmap(0, nullptr);
+    g_raygen_sbt_storage->SetName(L"RayGen SBT storage for primary ray");
 
     sbt_desc.Width = 64;
     CE(g_device12->CreateCommittedResource(
@@ -805,6 +989,7 @@ void CreateShaderBindingTable()
     g_hit_sbt_storage->Map(0, nullptr, (void**)&mapped);
     memcpy(mapped, hitgroup_id, D3D12_SHADER_IDENTIFIER_SIZE_IN_BYTES);
     g_hit_sbt_storage->Unmap(0, nullptr);
+    g_hit_sbt_storage->SetName(L"Hit group SBT storage for primary ray");
 
     CE(g_device12->CreateCommittedResource(
         &heap_props, D3D12_HEAP_FLAG_NONE, &sbt_desc, D3D12_RESOURCE_STATE_GENERIC_READ, nullptr, IID_PPV_ARGS(&g_miss_sbt_storage)));
@@ -957,19 +1142,64 @@ void Render()
         g_command_list->EndQuery(g_query_heap, D3D12_QUERY_TYPE_TIMESTAMP, 1);
         g_command_list->ResolveQueryData(g_query_heap, D3D12_QUERY_TYPE_TIMESTAMP, 0, 2, g_query_readback_buffer, 0);
 
-        barrier_rt_out.Transition.StateBefore = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
-        barrier_rt_out.Transition.StateAfter  = D3D12_RESOURCE_STATE_COPY_SOURCE;
-        g_command_list->ResourceBarrier(1, &barrier_rt_out);
+        if (0)  // COPY
+        {
+            barrier_rt_out.Transition.StateBefore = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+            barrier_rt_out.Transition.StateAfter  = D3D12_RESOURCE_STATE_COPY_SOURCE;
+            g_command_list->ResourceBarrier(1, &barrier_rt_out);
 
-        barrier_rtv.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
-        barrier_rtv.Transition.StateAfter  = D3D12_RESOURCE_STATE_COPY_DEST;
-        g_command_list->ResourceBarrier(1, &barrier_rtv);
+            barrier_rtv.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+            barrier_rtv.Transition.StateAfter  = D3D12_RESOURCE_STATE_COPY_DEST;
+            g_command_list->ResourceBarrier(1, &barrier_rtv);
 
-        g_command_list->CopyResource(g_rendertargets[g_frame_index], g_rt_output_resource);
+            g_command_list->CopyResource(g_rendertargets[g_frame_index], g_rt_output_resource);
 
-        barrier_rtv.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
-        barrier_rtv.Transition.StateAfter  = D3D12_RESOURCE_STATE_PRESENT;
-        g_command_list->ResourceBarrier(1, &barrier_rtv);
+            barrier_rtv.Transition.StateBefore = D3D12_RESOURCE_STATE_COPY_DEST;
+            barrier_rtv.Transition.StateAfter  = D3D12_RESOURCE_STATE_PRESENT;
+            g_command_list->ResourceBarrier(1, &barrier_rtv);
+        }
+        else  // FSQUAD
+        {
+            barrier_rt_out.Transition.StateBefore = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+            barrier_rt_out.Transition.StateAfter  = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+            g_command_list->ResourceBarrier(1, &barrier_rt_out);
+
+            g_command_list->SetGraphicsRootSignature(g_rootsig_fsquad);
+            g_command_list->SetPipelineState(g_pipeline_fsquad);
+            D3D12_GPU_DESCRIPTOR_HANDLE srv_uav_cbv_fsquad_handle(g_srv_uav_cbv_heap_fsquad->GetGPUDescriptorHandleForHeapStart());
+            g_command_list->SetGraphicsRootDescriptorTable(0, srv_uav_cbv_fsquad_handle);
+            g_command_list->SetDescriptorHeaps(1, &g_srv_uav_cbv_heap_fsquad);
+
+            D3D12_VIEWPORT viewport{};
+            viewport.TopLeftX = 0;
+            viewport.TopLeftY = 0;
+            viewport.Width    = WIN_W;
+            viewport.Height   = WIN_H;
+            viewport.MinDepth = 0;
+            viewport.MaxDepth = 1;
+
+            D3D12_RECT scissor{};
+            scissor.left   = 0;
+            scissor.top    = 0;
+            scissor.right  = WIN_W;
+            scissor.bottom = WIN_H;
+            g_command_list->RSSetViewports(1, &viewport);
+            g_command_list->RSSetScissorRects(1, &scissor);
+
+            g_command_list->IASetPrimitiveTopology(D3D_PRIMITIVE_TOPOLOGY_TRIANGLELIST);
+            g_command_list->IASetVertexBuffers(0, 1, &g_fsquad_vbv);
+
+            g_command_list->OMSetRenderTargets(1, &handle_rtv, false, nullptr);
+            g_command_list->DrawInstanced(3, 1, 0, 0);
+
+            barrier_rt_out.Transition.StateBefore  = D3D12_RESOURCE_STATE_PIXEL_SHADER_RESOURCE;
+            barrier_rt_out.Transition.StateAfter = D3D12_RESOURCE_STATE_UNORDERED_ACCESS;
+            g_command_list->ResourceBarrier(1, &barrier_rt_out);
+
+            barrier_rtv.Transition.StateBefore = D3D12_RESOURCE_STATE_RENDER_TARGET;
+            barrier_rtv.Transition.StateAfter  = D3D12_RESOURCE_STATE_PRESENT;
+            g_command_list->ResourceBarrier(1, &barrier_rtv);
+        }
     }
     else
     {
@@ -998,13 +1228,13 @@ void Render()
     if (g_frame_time.ShouldUpdate())
     {
         std::stringstream ss;
-        ss << "MyRRAPlayground ";
         if (!g_as_built)
         {
-            ss << "Building AS ...";
+            // Update in building procedure
         }
         else
         {
+            ss << "MyRRAPlayground ";
             ss << std::setprecision(4) << (g_frame_time.GetFrameTime() * 1000) << "ms/frame";
             if (g_use_ao)
             {
@@ -1014,8 +1244,8 @@ void Render()
             {
                 ss << " primary ray";
             }
+            glfwSetWindowTitle(g_window, ss.str().c_str());
         }
-        glfwSetWindowTitle(g_window, ss.str().c_str());
     }
 }
 
@@ -1105,6 +1335,7 @@ void CreateAS(const std::vector<std::vector<Vertex>>& vertices, const std::vecto
         printf("BLAS[%u] prebuild info:", i_blas);
         printf(" Scratch: %d", int(pb_info.ScratchDataSizeInBytes));
         printf(", Result : %d\n", int(pb_info.ResultDataMaxSizeInBytes));
+        glfwSetWindowTitle(g_window, (std::string("Building BLAS ") + std::to_string(i_blas+1) + "/" + std::to_string(vertices.size())).c_str());
 
         D3D12_RESOURCE_DESC scratch_desc{};
         scratch_desc.Alignment          = 0;
@@ -1160,7 +1391,7 @@ void CreateAS(const std::vector<std::vector<Vertex>>& vertices, const std::vecto
         g_command_queue->ExecuteCommandLists(1, (ID3D12CommandList* const*)(&g_command_list1));
         WaitForPreviousFrame();
 
-        blas_scratch->Release();
+        //blas_scratch->Release();
         blases.push_back(blas_result);
     }
 
